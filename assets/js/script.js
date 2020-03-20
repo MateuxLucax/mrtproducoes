a2b18215a8934d0609c43307cba41a76d5748bed
Mateus Lucas <mateusslucass840@gmail.com>
2020-03-19 23:57:13 -0300

    responsive

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+window.setTimeout(function() {
+    document.getElementById('navigation').style.visibility = 'visible';
+}, 400);
--- index.php
+++ index.php
@@ -7,21 +7,29 @@
     $title = 'Página Inicial';
     $root_path = './'; // Path relative to the root of the project
                                                                             // Params needed
-    $header = file_get_contents($root_path. "html/components/header.html"); // {hero_mod} / {title}
+    $nav = file_get_contents($root_path. "html/components/nav.html"); // {hero_mod} / {title}
+    $header = file_get_contents($root_path. "html/components/header.html"); // {root_path} / {1}, {2}, {3}, {4}, {5} --> Active page. Use "active" for activation 
     $footer = file_get_contents($root_path. "html/components/footer.html"); // {theme}
-    $page = file_get_contents($root_path. "html/template.html"); // {title} / {root_path} / {page_layout} / {{header}} / {{content}} / {{footer}}
+    $page = file_get_contents($root_path. "html/template.html"); // {title} / {root_path} / {page_layout} / {{header}} / {{content}} / {{footer}} / {{script}}
+    $script = file_get_contents($root_path. "assets/js/script.js");
     $content = file_get_contents($root_path. "main.html");
 
     // Custom params
     $hero_mod = "hero--home";
     $page_layout = "home";
+    $active = "active";
 
+    $page = str_replace("{title}", $title, $page);
+    $page = str_replace("{{nav}}", $nav, $page);
+    $title = "MRT Produções";
     $page = str_replace("{{header}}", $header, $page);
+    $page = str_replace("{title}", $title, $page);
     $page = str_replace("{{footer}}", $footer, $page);
+    $page = str_replace("{{script}}", $script, $page);
     $page = str_replace("{{content}}", $content, $page);
     $page = str_replace("{root_path}", $root_path, $page);
     $page = str_replace("{hero_mod}", $hero_mod, $page);
-    $page = str_replace("{title}", $title, $page);
+    $page = str_replace("{1}", $active, $page);
     $page = str_replace("{page_layout}", $page_layout, $page);
     
     print($page);
