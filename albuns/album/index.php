264daeaf310bb2b639943e3b142beb97c00b0c4d
Mateus Lucas <mateusslucass840@gmail.com>
2020-03-30 15:09:40 -0300

    Album page finished
    Ready for approval

--- albuns/album/index.php
+++ albuns/album/index.php
@@ -4,22 +4,19 @@
     // {} --> parameters
 
     // Startup variables
-    $title = 'Álbuns';
-    $description = 'Fotografamos ensaios externos, pré eventos e produtos!';
-    $root_path = '../'; // Path relative to the root of the project
+    $title = 'Álbum';
+    $description = 'Álbum de Fulano!';
+    $root_path = '../../'; // Path relative to the root of the project
                                                                             // Params needed
-    $nav = file_get_contents($root_path. "html/components/nav.html"); // {hero_mod} / {title}
-    $header = file_get_contents($root_path. "html/components/header.html"); // {root_path} / {1}, {2}, {3}, {4}, {5} --> Active page. Use "active" for activation 
+    $nav = file_get_contents($root_path. "html/components/nav.html"); // {root_path} / {1}, {2}, {3}, {4}, {5} --> Active page. Use "active" for activation 
+    $header = '';
     $footer = file_get_contents($root_path. "html/components/footer.html"); // {theme}
     $page = file_get_contents($root_path. "html/template.html"); // {title} / {root_path} / {page_layout} / {{header}} / {{content}} / {{footer}} / {{script}}
-    $script = file_get_contents($root_path. "assets/js/script.js");
-    $content = file_get_contents($root_path. "albuns/main.html");
+    $script = file_get_contents($root_path. "albuns/album/script.js");
+    $content = file_get_contents($root_path. "albuns/album/main.html");
 
     // Custom params
-    $hero_mod = "hero--albums";
-    $page_layout = "albums";
-    $active = "active";
-    $hero_logo = '';
+    $page_layout = "album";
     $theme = "footer--purple";
 
     $page = str_replace("{title}", $title, $page);
@@ -31,9 +28,6 @@
     $page = str_replace("{{script}}", $script, $page);
     $page = str_replace("{{content}}", $content, $page);
     $page = str_replace("{root_path}", $root_path, $page);
-    $page = str_replace("{hero_mod}", $hero_mod, $page);
-    $page = str_replace("{hero_logo}", $hero_logo, $page);
-    $page = str_replace("{2}", $active, $page);
     $page = str_replace("{theme}", $theme, $page);
     $page = str_replace("{page_layout}", $page_layout, $page);
     
